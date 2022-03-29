fb336c876ca15c8bd536ca124f22222fc568d961
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2022-03-29 11:30:03 +0200

    forSchleife

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'models/transaction.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,6 +14,30 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 'meggeboocke',
+        title: 'MacBook',
+        amount: -400.00,
+        date: DateTime.now())
+  ];
+
+  void removeElement({required id}) {
+    String removeId = id;
+
+    for (var i = 0; i < transactions.length; i++) {
+      print(transactions[i]);
+    }
+  }
+
+  void addTransaction({required title, required amount}) {}
+
+  // late String titleInput;
+  // late String amountInput;
+
+  final titleInputController = TextEditingController();
+  final amountInputController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,17 +46,95 @@ class _AppState extends State<App> {
             title: Text('Expenses Tracker'),
           ),
           body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Container(
                 child: Card(
                   child: Text('Chart'),
                 ),
-                width: double.infinity,
                 height: 50,
               ),
               Card(
-                child: Text('Transactions'),
-              )
+                  child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(labelText: 'Title:'),
+                            controller: titleInputController,
+                            // onChanged: (val) => titleInput = val,
+                          ),
+                          TextField(
+                            decoration: InputDecoration(labelText: 'Amount:'),
+                            controller: amountInputController,
+                            // onChanged: (val) => amountInput = val,
+                          ),
+                          ElevatedButton(
+                              onPressed: () {
+                                print(titleInputController.text);
+                              },
+                              child: Text('Add Transaction'))
+                        ],
+                      ),
+                      margin: EdgeInsets.fromLTRB(7, 5, 7, 1))),
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: transactions.map((tx) {
+                    return Card(
+                        child: Row(
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              tx.title,
+                              style: TextStyle(fontSize: 25),
+                            ),
+                            tx.amount < 0
+                                ? Container(
+                                    child: Text(
+                                      (tx.amount.toString()) + ' €',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          color:
+                                              Color.fromARGB(255, 255, 0, 0)),
+                                    ),
+                                    padding: EdgeInsets.fromLTRB(0, 10, 30, 5),
+                                  )
+                                : Container(
+                                    child: Text(
+                                      (tx.amount.toString()) + ' €',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          color:
+                                              Color.fromARGB(255, 0, 185, 0)),
+                                    ),
+                                    padding: EdgeInsets.fromLTRB(0, 0, 40, 0),
+                                  )
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Container(
+                                child: Text(
+                                  DateFormat('d MMM y  H:mm').format(tx.date),
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color:
+                                          Color.fromARGB(255, 123, 124, 133)),
+                                ),
+                                padding: EdgeInsets.fromLTRB(105, 0, 0, 40)),
+                            Container(
+                              child: ElevatedButton(
+                                child: Text('Remove'),
+                                onPressed: () => removeElement(id: tx.id),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ));
+                  }).toList())
             ],
           )),
     );
